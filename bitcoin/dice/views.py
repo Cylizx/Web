33e6ce0d60287af9ffeeaba54e1c5ef1ab0dbfad
chenchenchen <chenxinyao13@mails.tsinghua.edu.cn>
2018-01-03 22:22:16 +0800

    react according to different people

--- bitcoin/dice/views.py
+++ bitcoin/dice/views.py
@@ -3,6 +3,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from dice import models
+import random
 
 # Create your views here.
 
@@ -37,3 +38,142 @@ def loading(request):
  
 def player(request):
     return render(request, 'player.html')
+
+def player_CXY(request):
+    return render(request, 'player_CXY.html')
+
+def player_LJS(request):
+    return render(request, 'player_LJS.html')
+
+def player_LQS(request):
+    return render(request, 'player_LQS.html')
+
+def player_SN(request):
+    return render(request, 'player_SN.html')
+
+def player_XC(request):
+    return render(request, 'player_XC.html')
+
+def player_ZZX(request):
+    return render(request, 'player_ZZX.html')
+
+def judge(request):
+
+    if 'big' in request.GET:
+        a = [8, 15]
+        message = '你赌的是大'
+    if 'small' in request.GET:
+        a = [0, 7]
+        message = '你赌的是小'
+    c = random.randint(0,15)
+    if c in a:
+        result = 1
+
+    else:
+        result = 0
+
+    return HttpResponse(message)
+
+def judge_CXY(request):
+
+    if 'big' in request.GET:
+        a = [8, 15]
+        message = 'CXY赌的是大'
+    if 'small' in request.GET:
+        a = [0, 7]
+        message = 'CXY赌的是小'
+    c = random.randint(0,15)
+    if c in a:
+        result = 1
+
+    else:
+        result = 0
+
+    return HttpResponse(message)
+
+def judge_LJS(request):
+
+    if 'big' in request.GET:
+        a = [8, 15]
+        message = 'LJS赌的是大'
+    if 'small' in request.GET:
+        a = [0, 7]
+        message = 'LJS赌的是小'
+    c = random.randint(0,15)
+    if c in a:
+        result = 1
+
+    else:
+        result = 0
+
+    return HttpResponse(message)
+
+def judge_LQS(request):
+
+    if 'big' in request.GET:
+        a = [8, 15]
+        message = 'LQS赌的是大'
+    if 'small' in request.GET:
+        a = [0, 7]
+        message = 'LQS赌的是小'
+    c = random.randint(0,15)
+    if c in a:
+        result = 1
+
+    else:
+        result = 0
+
+    return HttpResponse(message)
+
+def judge_SN(request):
+
+    if 'big' in request.GET:
+        a = [8, 15]
+        message = 'SN赌的是大'
+    if 'small' in request.GET:
+        a = [0, 7]
+        message = 'SN赌的是小'
+    c = random.randint(0,15)
+    if c in a:
+        result = 1
+
+    else:
+        result = 0
+
+    return HttpResponse(message)
+
+def judge_XC(request):
+
+    if 'big' in request.GET:
+        a = [8, 15]
+        message = 'XC赌的是大'
+    if 'small' in request.GET:
+        a = [0, 7]
+        message = 'XC赌的是小'
+    c = random.randint(0,15)
+    if c in a:
+        result = 1
+
+    else:
+        result = 0
+
+    return HttpResponse(message)
+
+def judge_ZZX(request):
+
+    if 'big' in request.GET:
+        a = [8, 15]
+        message = 'ZZX赌的是大'
+    if 'small' in request.GET:
+        a = [0, 7]
+        message = 'ZZX赌的是小'
+    c = random.randint(0,15)
+    if c in a:
+        result = 1
+
+    else:
+        result = 0
+
+    return HttpResponse(message)
+
+
