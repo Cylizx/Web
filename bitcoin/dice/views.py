d7bfc98d218e11f518a45eb1c51b68c32eec2273
simplelife1314 <xuanc_orange@foxmail.com>
2018-01-03 22:05:49 +0800

    fix loading

--- bitcoin/dice/views.py
+++ bitcoin/dice/views.py
@@ -3,52 +3,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from dice import models
-import random
 
 # Create your views here.
 
-# coding=utf-8
-
-def func(request):
-    # 如果Ajax使用了GET方法，把下面的POST换成GET即可
-    name = request.POST['name']
-    age = request.POST['age']
-
-def player(request):
-    return render(request, 'player.html')
-    
 def home(request):
+    return render(request, 'home.html')
+
+def result(request):
     p1 = models.Player.objects.get(id=1)
     p2 = models.Player.objects.get(id=2)
     p3 = models.Player.objects.get(id=3)
     p4 = models.Player.objects.get(id=4)
     p5 = models.Player.objects.get(id=5)
     p6 = models.Player.objects.get(id=6)
-    return render(request, 'home.html',{"p1":p1,"p2":p2,"p3":p3,"p4":p4,"p5":p5,"p6":p6,})
+    return render(request, 'result.html',{"p1":p1,"p2":p2,"p3":p3,"p4":p4,"p5":p5,"p6":p6,})
+
+def loading(request):
+    flag = False
+    message = ""
+    p1 = models.Player.objects.get(id=1)
+    p2 = models.Player.objects.get(id=2)
+    p3 = models.Player.objects.get(id=3)
+    p4 = models.Player.objects.get(id=4)
+    p5 = models.Player.objects.get(id=5)
+    p6 = models.Player.objects.get(id=6) 
+    if p1.status == True and p2.status == True and p3.status == True and p4.status == True and p5.status == True and p6.status == True:
+        flag = True
+    else: flag = False
+    if flag==False:
+        message = "正在等待开奖，请稍后……"
+    else: message = "结果已经出来了，点击下方按钮查看"
+    return render(request, 'loading.html',{"status":message})
  
-def index(request):
-    return render(request, 'index.html')
-     
-def add(request):
-    a = request.GET['a']
-    b = request.GET['b']
-    a = int(a)
-    b = int(b)
-    return HttpResponse(str(a+b))
-
-def judge(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = '你赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = '你赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
+def player(request):
+    return render(request, 'player.html')
