25a236ae7f8eb9d9b8b2b953f84df07bdb04de8e
chenchenchen <chenxinyao13@mails.tsinghua.edu.cn>
2018-01-04 01:08:11 +0800

    loading & result

--- bitcoin/dice/views.py
+++ bitcoin/dice/views.py
@@ -11,169 +11,40 @@ def home(request):
     return render(request, 'home.html')
 
 def result(request):
-    p1 = models.Player.objects.get(id=1)
-    p2 = models.Player.objects.get(id=2)
-    p3 = models.Player.objects.get(id=3)
-    p4 = models.Player.objects.get(id=4)
-    p5 = models.Player.objects.get(id=5)
-    p6 = models.Player.objects.get(id=6)
-    return render(request, 'result.html',{"p1":p1,"p2":p2,"p3":p3,"p4":p4,"p5":p5,"p6":p6,})
+    p = []
+    i = 0
+    print (type(models.Player.objects.get(id=1)))
+    for i in range(6):
+        p.append(models.Player.objects.get(id=i+1))
+        models.Player.objects.filter(id=i+1).update(status=False)
+    return render(request, 'result.html',{"p1":p[0],"p2":p[1],"p3":p[2],"p4":p[3],"p5":p[4],"p6":p[5],})
 
 def loading(request):
-    flag = False
+    flag = True
     message = ""
-    p1 = models.Player.objects.get(id=1)
-    p2 = models.Player.objects.get(id=2)
-    p3 = models.Player.objects.get(id=3)
-    p4 = models.Player.objects.get(id=4)
-    p5 = models.Player.objects.get(id=5)
-    p6 = models.Player.objects.get(id=6) 
-    if p1.status == True and p2.status == True and p3.status == True and p4.status == True and p5.status == True and p6.status == True:
-        flag = True
-    else: flag = False
+
+    # TBD: judge if there is already a gambling number
+
+    prev_url = request.META['HTTP_REFERER']
+    user_id = prev_url.split('/')[3]
+    if (user_id != 'loading'):
+        user_id = int(user_id)
+        models.Player.objects.filter(id=user_id).update(status=True)
+        if 'big' in request.GET:
+            models.Player.objects.filter(id=user_id).update(result=True)
+        if 'small' in request.GET:
+            models.Player.objects.filter(id=user_id).update(result=False)
+
+    for i in range(6):
+        p = models.Player.objects.get(id=i+1)
+        if p.status == False:
+            flag = False
+            break
+
     if flag==False:
         message = "正在等待开奖，请稍后……"
     else: message = "结果已经出来了，点击下方按钮查看"
-    return render(request, 'loading.html',{"status":message})
+    return render(request, 'loading.html', {"status":message})
  
 def player(request):
     return render(request, 'player.html')
-
-def player_CXY(request):
-    return render(request, 'player_CXY.html')
-
-def player_LJS(request):
-    return render(request, 'player_LJS.html')
-
-def player_LQS(request):
-    return render(request, 'player_LQS.html')
-
-def player_SN(request):
-    return render(request, 'player_SN.html')
-
-def player_XC(request):
-    return render(request, 'player_XC.html')
-
-def player_ZZX(request):
-    return render(request, 'player_ZZX.html')
-
-def judge(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = '你赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = '你赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
-
-def judge_CXY(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = 'CXY赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = 'CXY赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
-
-def judge_LJS(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = 'LJS赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = 'LJS赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
-
-def judge_LQS(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = 'LQS赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = 'LQS赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
-
-def judge_SN(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = 'SN赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = 'SN赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
-
-def judge_XC(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = 'XC赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = 'XC赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
-
-def judge_ZZX(request):
-
-    if 'big' in request.GET:
-        a = [8, 15]
-        message = 'ZZX赌的是大'
-    if 'small' in request.GET:
-        a = [0, 7]
-        message = 'ZZX赌的是小'
-    c = random.randint(0,15)
-    if c in a:
-        result = 1
-
-    else:
-        result = 0
-
-    return HttpResponse(message)
-
-
